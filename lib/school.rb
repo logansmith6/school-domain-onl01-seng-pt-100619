2c888ccf9ff6da01f5193e7a19a82dbc9e7e62ca
Logan Smith <logsmith06@gmail.com>
2019-11-06 21:49:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,6 +10,13 @@ class School
     @name = name
   end
 
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+        @roster[grade] << name
+    else
+        @roster[grade] = [name]
+      end
+    end
 
 
 end
